05a8bf7791a42cd06d2399a07e102a49a44703f2
Wuzzy <almikes@aol.com>
2016-11-09 21:16:53 +0100

    Make sand non-diggable

--- mods/default/nodes.lua
+++ mods/default/nodes.lua
@@ -81,7 +81,7 @@ minetest.register_node("default:sand", {
 	description = S("sand"),
 	tiles = {"default_sand.png"},
 	is_ground_content = true,
-	groups = {crumbly=3, falling_node=1, sand=1},
+	groups = {immortal=1, falling_node=1},
 	sounds = default.node_sound_sand_defaults(),
 })
 
